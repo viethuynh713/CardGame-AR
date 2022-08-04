ec3d812cfb0dfd902d1233ce98ab5d8e64407fcd
hanhathuy2612 <66113968+viethuynh713@users.noreply.github.com>
2022-08-04 14:08:14 +0700

    update

--- Assets/Scripts/InitBoard.cs
+++ Assets/Scripts/InitBoard.cs
@@ -19,9 +19,8 @@ public class InitBoard : MonoBehaviour
                 var idx = (z + 1) * 8 + x + 3;
                 var obj = PhotonNetwork.Instantiate("Red_PlayingCards_" + GameManager.instance.listCard[idx].suit + GameManager.instance.listCard[idx].rank, gameObject.transform.position, Quaternion.identity);
                 obj.transform.SetParent(transform);
-                var objCard = obj.AddComponent<Card>();
-                objCard.suit = GameManager.instance.listCard[idx].suit;
-                objCard.rank = GameManager.instance.listCard[idx].rank;
+                obj.GetComponent<Card>().suit = GameManager.instance.listCard[idx].suit;
+                obj.GetComponent<Card>().rank = GameManager.instance.listCard[idx].rank;
 
                 obj.transform.position = new Vector3(0, 0.85f, 0);
                 obj.GetComponent<Card>().MoveTo(new Vector3((float)((x - 1) * 0.2 + 0.1), 0.85f, (float)((z - 1) * 0.3 + 0.15)));
